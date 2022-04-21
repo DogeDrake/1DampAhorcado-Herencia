eaeb294abd7b28e9798b636a771c5e120f82d45e
Luis González Rodriguez <lugon2002@gmail.com>
2022-04-21 12:41:45 +0200

    Root Commit

--- src/Ahorcado/Marcador.java
+++ src/Ahorcado/Marcador.java
@@ -1,80 +1,141 @@
 package Ahorcado;
+
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Marcador {
 	private int numAciertos, numFallos, intentos, puntos;
-	private ArrayList <Character> listaAciertos, listaFallos;
-	
+	private ArrayList<Character> listaAciertos, listaFallos;
+
 	/**
-	 * Constructor que inicializa el juego del ahorcado 
+	 * Constructor que inicializa el juego del ahorcado
+	 * 
 	 * @param intentos número de fallos permitidos para adivinar la palabra
 	 */
-	public Marcador (int intentos) {
-		this.numAciertos=0;
-		this.numFallos=0;
-		this.intentos=intentos;
-		this.puntos=0;
-		listaAciertos = new ArrayList<Character> ();
-		listaFallos = new ArrayList<Character> ();
-	}
-	
-	/**
-	 * Método de tipo set que actualiza la puntuación del marcador con la fórmula:
-	 * 5 puntos por cada acierto y 1 punto menos por cada fallo 
-	 */
-	private void actualizar (){
-		//IMPLEMENTAR;
-	}
-	
-	/**
-	 * Método que sirve para indicarle al jugador qué letras ha acertado y qué letras ha fallado
-	 * Cada una de ellas sólo se imprime en caso de que haya elementos dentro. 
-	 * La sintaxis es la siguiente: 
-	 * -> Letras acertadas: A B C... en caso de que el jugador haya indicado esas letras y se encuentren en la palabra
-	 * -> Letras falladas: X Y Z... en caso de que el jugador haya indicado esas letras y se NO encuentren en la palabra
-	 */
-	private void mostrarAciertoyFallos (){
-		//IMPLEMENTAR
-	}
-	
-	/**
-	 * Método que dibuja el Marcador, que incluye los aciertos y fallos (en caso de que existan), 
-	 * el número de intentos que quedan con la siguiente sintaxis:
-	 * -> Intentos restantes = X, siendo X el número de intentos (posibles fallos) que quedan
-	 * y los puntos, con la siguiente sintaxis:
-	 * -> PUNTOS =  Y, siendo Y el número total de puntos
-	 */
-	public void mostrarMarcador() {
-		//IMPLEMENTAR
-	}
-	
-	
-	/**
-	 * Método que incrementa el número de aciertos, actualiza los puntos y añade la letra a la lista de Aciertos
-	 * @param letra que ha introducido el jugador 
-	 * @see listaAciertos
-	 * @see actualizar()
-	 */
-	public void acertar (char letra){
-		//IMPLEMENTAR
+	public Marcador(int intentos) {
+		this.numAciertos = 0;
+		this.numFallos = 0;
+		this.intentos = intentos;
+		this.puntos = 0;
+		listaAciertos = new ArrayList<Character>();
+		listaFallos = new ArrayList<Character>();
 	}
 
 	/**
-	 * Método que incrementa el número de fallos, disminuye el número de intentos, actualiza los puntos y añade la letra a la lista de Fallos
-	 * @param letra que ha introducido el jugador 
+	 * Método de tipo set que actualiza la puntuación del marcador con la fórmula: 5
+	 * puntos por cada acierto y 1 punto menos por cada fallo
+	 */
+	private void actualizar() {
+		// IMPLEMENTAR;
+		int AciertosPasado = 0;
+		int FallosPasado = 0;
+		if (numAciertos > AciertosPasado) {
+			setPuntos(puntos + 5);
+			AciertosPasado = numAciertos;
+		}
+		if (numFallos > FallosPasado) {
+			setPuntos(puntos - 1);
+			FallosPasado = numFallos;
+		}
+	}
+
+	/**
+	 * Método que sirve para indicarle al jugador qué letras ha acertado y qué
+	 * letras ha fallado Cada una de ellas sólo se imprime en caso de que haya
+	 * elementos dentro. La sintaxis es la siguiente: -> Letras acertadas: A B C...
+	 * en caso de que el jugador haya indicado esas letras y se encuentren en la
+	 * palabra -> Letras falladas: X Y Z... en caso de que el jugador haya indicado
+	 * esas letras y se NO encuentren en la palabra
+	 */
+	private void mostrarAciertoyFallos() {
+		// IMPLEMENTAR
+		if (listaAciertos.size() > 0) {
+			System.out.println("-> Letras acertadas: ");
+			for (Character character : listaAciertos) {
+				System.out.print(character);
+			}
+			System.out.println("");
+		}
+		if (listaFallos.size() > 0) {
+			System.out.println("-> Letras falladas: ");
+			for (Character character : listaFallos) {
+				System.out.print(character);
+			}
+			System.out.println("");
+		}
+	}
+
+	/**
+	 * Método que dibuja el Marcador, que incluye los aciertos y fallos (en caso de
+	 * que existan), el número de intentos que quedan con la siguiente sintaxis: ->
+	 * Intentos restantes = X, siendo X el número de intentos (posibles fallos) que
+	 * quedan y los puntos, con la siguiente sintaxis: -> PUNTOS = Y, siendo Y el
+	 * número total de puntos
+	 */
+	public void mostrarMarcador() {
+		// IMPLEMENTAR
+		System.out.println("*****************************************");
+		System.out.println("********** MARCADOR *********************");
+		System.out.println("*****************************************");
+		System.out.println("-> Intentos restantes = " + intentos);
+		System.out.println("-> PUNTOS = " + puntos);
+	}
+
+	/**
+	 * Método que incrementa el número de aciertos, actualiza los puntos y añade la
+	 * letra a la lista de Aciertos
+	 * 
+	 * @param letra que ha introducido el jugador
+	 * @see listaAciertos
+	 * @see actualizar()
+	 */
+	public void acertar(char letra) {
+		// IMPLEMENTAR
+		numAciertos++;
+		letra = Character.toUpperCase(letra);
+		actualizar();
+		listaAciertos.add(letra);
+	}
+
+	/**
+	 * Método que incrementa el número de fallos, disminuye el número de intentos,
+	 * actualiza los puntos y añade la letra a la lista de Fallos
+	 * 
+	 * @param letra que ha introducido el jugador
 	 * @see listaFallos
 	 * @see actualizar()
 	 */
-	public void fallar(char letra){
-		//IMPLEMENTAR
+	public void fallar(char letra) {
+		// IMPLEMENTAR
+		numFallos++;
+		intentos--;
+		letra = Character.toUpperCase(letra);
+		actualizar();
+		listaFallos.add(letra);
 	}
-	
+
 	/**
-	 * Método que sirve para saber si el juego se ha terminado por haber agotado el número de intentos disponibles
-	 * @return true cuando se han agotado el número de intentos posibles (es igual a cero)
+	 * Método que sirve para saber si el juego se ha terminado por haber agotado el
+	 * número de intentos disponibles
+	 * 
+	 * @return true cuando se han agotado el número de intentos posibles (es igual a
+	 *         cero)
 	 */
-	public boolean finJuego(){
-		//IMPLEMENTAR
-		return false;
+	public boolean finJuego() {
+		// IMPLEMENTAR
+		if (intentos == 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
+
+	public int getPuntos() {
+		return puntos;
+	}
+
+	public void setPuntos(int puntos) {
+		this.puntos = puntos;
+	}
+
 }
--- src/Ahorcado/Palabra.java
+++ src/Ahorcado/Palabra.java
@@ -4,61 +4,93 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Palabra {
-	//Para almacenar la palabra que hay que adivinar
+	// Para almacenar la palabra que hay que adivinar
 	private String p_oculta;
-	//Para almacenar la parte que se ha adivinado de la palabra
+	// Para almacenar la parte que se ha adivinado de la palabra
 	private char[] p_visible;
+
 	/**
-	 * Constructor que genera una palabra de forma aleatoria (p_oculta)
-	 * también inicializa un Array (p_visible) con "_" en cada una de las letras
-	 * estos subrayados se irán sustituyendo según se vayan adividando las letras
+	 * Constructor que genera una palabra de forma aleatoria (p_oculta) también
+	 * inicializa un Array (p_visible) con "_" en cada una de las letras estos
+	 * subrayados se irán sustituyendo según se vayan adividando las letras
 	 */
 	public Palabra() {
-		String[] palabras = { "BATMAN", "SUPERMAN", "HULK", "CENICIENTA",
-				"SPIDERMAN", "PINOCHO", "LOBEZNO" };
-		int n = (int)Math.floor(Math.random()* palabras.length);
+		String[] palabras = { "BATMAN", "SUPERMAN", "HULK", "CENICIENTA", "SPIDERMAN", "PINOCHO", "LOBEZNO" };
+		int n = (int) Math.floor(Math.random() * palabras.length);
 		p_oculta = palabras[n];
 		p_visible = new char[p_oculta.length()];
 		for (int i = 0; i < p_visible.length; i++) {
 			p_visible[i] = '_';
 		}
 	}
-	
+
 	/**
 	 * Método que sirve para saber si se ha adivinado la palabra
-	 * @return devuelve true si la palabra oculta (p_oculta) tiene el mismo contenido que la parte visible (p_visible), false en caso contrario
+	 * 
+	 * @return devuelve true si la palabra oculta (p_oculta) tiene el mismo
+	 *         contenido que la parte visible (p_visible), false en caso contrario
 	 */
-	public boolean esta_adivinada(){
-		//IMPLEMENTAR
-		return false;
+	public boolean esta_adivinada() {
+		// IMPLEMENTAR
+		if (p_oculta.equals(p_visible)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	/**
-	 * Método que sirve para mostrar la parte visible de la palabra (p_visible)
-	 * Se imprimen las letras separadas por un espacio y un salto de línea al final
+	 * Método que sirve para mostrar la parte visible de la palabra (p_visible) Se
+	 * imprimen las letras separadas por un espacio y un salto de línea al final
 	 */
 	public void mostrar_visible() {
-		//IMPLEMENTAR
+		// IMPLEMENTAR
+		for (int i = 0; i < p_visible.length; i++) {
+			System.out.print(" " + p_visible[i] + " ");
+		}
+		System.out.println("");
 	}
 
-	
 	/**
-	 * Método que busca las letras en la palabra (p_oculta) y además, en caso de encontrarla la pone en la parte visible (p_visible) en su posición correcta
-	 * de este modo cuando se imprime la parte visible  
+	 * Método que busca las letras en la palabra (p_oculta) y además, en caso de
+	 * encontrarla la pone en la parte visible (p_visible) en su posición correcta
+	 * de este modo cuando se imprime la parte visible
+	 * 
 	 * @param c El carácter a buscar en la palabra completa (p_oculta)
-	 * @return Se devuelve el número de aciertos, es decir, el número de veces que aparece c en el palabra (p_oculta)
+	 * @return Se devuelve el número de aciertos, es decir, el número de veces que
+	 *         aparece c en el palabra (p_oculta)
 	 */
 	public int buscar(char c) {
-		//IMPLEMENTAR
-		return 0;
+		// IMPLEMENTAR
+		char caroculto;
+		int x = 0;
+		for (int i = 0; i < p_oculta.length(); i++) {
+			caroculto = p_oculta.charAt(i);
+			if (caroculto == c) {
+				p_visible[i] = c;
+				x++;
+			}
+		}
+		return x;
 	}
-	
+
 	/**
-	 * Método que cuenta el número de letras diferentes que tiene la palabra (p_oculta)
+	 * Método que cuenta el número de letras diferentes que tiene la palabra
+	 * (p_oculta)
+	 * 
 	 * @return número de letras diferentes
 	 */
-    public int letras_diferentes(){
-    	//IMPLEMENTAR
-    	return 0;
-    }
+	public int letras_diferentes() {
+		// IMPLEMENTAR
+		int nletras;
+		String lpalabra = "";
+		for (int i = 0; i < p_oculta.length(); i++) {
+			if (!lpalabra.contains(String.valueOf(p_oculta.charAt(i)))) {
+				lpalabra += p_oculta.charAt(i);
+			}
+		}
+		nletras = lpalabra.length();
+
+		return nletras;
+	}
 }
